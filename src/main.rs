e63ec426f10f7efa7dbebafe9e494431cf6ba5b6
qwertzer12 <qwertzer12@outlook.com>
2026-02-20 16:12:44 -0500

    Basic parts for ui and cli implemented

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use clap::{Parser, Subcommand};
+mod api;
+mod tui;
+
+#[derive(Parser)]
+#[command(name = "acdc")]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    T,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        None => tui::run().unwrap(),
+        Some(Commands::T) => println!("Subcommand was used"),
+    }
+    api::test();
 }
